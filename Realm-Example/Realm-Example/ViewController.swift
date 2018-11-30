461094fa043e14172112168445b8c0eb38406924
Romell Bolton <user.email>
2018-11-30 16:57:34 -0500

    Set up UI for Realm project

---
+++ Realm-Example/Realm-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Realm-Example
+//
+//  Created by Romell Bolton on 11/30/18.
+//  Copyright © 2018 Romell Bolton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
